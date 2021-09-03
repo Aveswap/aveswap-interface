602e8bbd48427a9987f1efcbbfbbb9d6cca2242a
WestCtrl <westd7396@gmail.com>
2021-09-03 21:54:59 +0200

    Preparations for aveswap core

--- src/hooks/useV2Pairs.ts
+++ src/hooks/useV2Pairs.ts
@@ -1,6 +1,6 @@
 import { Currency, CurrencyAmount, FACTORY_ADDRESS, Pair, computePairAddress } from '@aveswap/sdk'
 
-import IUniswapV2PairABI from '@sushiswap/core/abi/IUniswapV2Pair.json'
+import IUniswapV2PairABI from '@aveswap/core/abi/IUniswapV2Pair.json'
 import { Interface } from '@ethersproject/abi'
 import { useMemo } from 'react'
 import { useMultipleContractSingleData } from '../state/multicall/hooks'
