584b279e97e30ee1f9951aa44ab61e53c7eb5833
WestCtrl <westd7396@gmail.com>
2021-09-03 20:16:37 +0200

    Initial commit

---
+++ src/components/DoubleGlowShadow/index.tsx
@@ -0,0 +1,18 @@
+import { isMobile } from 'react-device-detect'
+import { FC } from 'react'
+import { classNames } from '../../functions'
+
+const DoubleGlowShadow: FC<{ className?: string }> = ({ children, className }) => {
+  if (isMobile) {
+    return <div className="shadow-swap">{children}</div>
+  }
+
+  return (
+    <div className={classNames(className, 'relative w-full max-w-2xl')}>
+      <div className="absolute top-1/4 -left-10 bg-image" />
+      <div className="relative filter drop-shadow">{children}</div>
+    </div>
+  )
+}
+
+export default DoubleGlowShadow
