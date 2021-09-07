2318b1f7061d95d6e99f26f6a26ef40374dda169
WestCtrl <westd7396@gmail.com>
2021-09-07 18:25:47 +0200

    Fix local storage between browser reloads

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,64 +1,37 @@
-import { Action, ThunkAction, configureStore } from '@reduxjs/toolkit'
+import { save, load } from 'redux-localstorage-simple'
+import { Action, ThunkAction, configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist'
 
 import reducer from './reducer'
-import storage from 'redux-persist/lib/storage'
+import localStorage from 'redux-persist/lib/storage'
 import { updateVersion } from './global/actions'
 import { useMemo } from 'react'
-
-let store
 
 const PERSISTED_KEYS: string[] = ['user', 'transactions', 'lists']
 
 const persistConfig: any = {
   key: 'root',
   whitelist: PERSISTED_KEYS,
-  storage,
+  storage: localStorage,
   stateReconciler: false,
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer)
 
-function makeStore(preloadedState = undefined) {
-  return configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
+const store = configureStore({
+  devTools: process.env.NODE_ENV !== 'production',
+  reducer: persistedReducer,
+  middleware: [...getDefaultMiddleware({
         thunk: true,
         immutableCheck: true,
         serializableCheck: {
           ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
         },
-      }),
-    devTools: process.env.NODE_ENV === 'development',
-    preloadedState,
-  })
-}
+      }), save({ states: PERSISTED_KEYS })],
+  preloadedState: load({ states: PERSISTED_KEYS }),
+})
 
-export const getOrCreateStore = (preloadedState = undefined) => {
-  let _store = store ?? makeStore(preloadedState)
-
-  // After navigating to a page with an initial Redux state, merge that state
-  // with the current state in the store, and create a new store
-  if (preloadedState && store) {
-    _store = makeStore({
-      ...store.getState(),
-      ...preloadedState,
-    })
-    // Reset the current store
-    store = undefined
-  }
-
-  // For SSG and SSR always create a new store
-  if (typeof window === 'undefined') return _store
-
-  // Create the store once in the client
-  if (!store) store = _store
-
-  return _store
-}
-
-store = getOrCreateStore()
+store.dispatch(updateVersion())
 
 // export function useStore(preloadedState) {
 //   const store = useMemo(() => getOrCreateStore(preloadedState), [preloadedState])
