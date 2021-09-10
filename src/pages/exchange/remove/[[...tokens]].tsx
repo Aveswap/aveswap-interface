5dc096d0d059f0d540e4320b90dc179b34e1ac04
WestCtrl <westd7396@gmail.com>
2021-09-10 18:20:59 +0200

    Fix permit on remove liquidity

--- src/pages/exchange/remove/[[...tokens]].tsx
+++ src/pages/exchange/remove/[[...tokens]].tsx
@@ -711,7 +711,7 @@ export default function Remove() {
     <Container id="remove-liquidity-page" className="py-4 space-y-4 md:py-8 lg:py-12" maxWidth="2xl">
       <Head>
         <title>Remove Liquidity | Ave</title>
-        <meta key="description" name="description" content="Remove liquidity from the SushiSwap AMM" />
+        <meta key="description" name="description" content="Remove liquidity from the Aveswap AMM" />
       </Head>
       <div className="px-4 mb-5">
         <NavLink href="/pool">
