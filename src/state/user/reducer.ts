68045bcdb8ed568a0c583ff79f99297d1b3cb350
WestCtrl <westd7396@gmail.com>
2021-10-25 12:45:11 +0200

    Use dark mode by default

--- src/state/user/reducer.ts
+++ src/state/user/reducer.ts
@@ -71,7 +71,7 @@ function pairKey(token0Address: string, token1Address: string) {
 }
 
 export const initialState: UserState = {
-  userDarkMode: null,
+  userDarkMode: true,
   matchesDarkMode: false,
   userExpertMode: false,
   userSingleHopOnly: false,
