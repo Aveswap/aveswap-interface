9336bbba752462aa7709296ce659e251e3ef6d5a
Dmytro <andcutmediagroup@gmail.com>
2021-12-15 15:34:34 +0200

    mainnet rpc update

--- src/config/rpc.ts
+++ src/config/rpc.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '@aveswapio/sdk'
 
 const rpc = {
-  [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/q1gSNoSMEzJms47Qn93f9-9Xg5clkmEC',
+  [ChainId.MAINNET]: 'https://eth-mainnet.alchemyapi.io/v2/9riPg_Q7St0jlVGtZXyl0l9q2nx2ZF3a',
   [ChainId.ROPSTEN]: 'https://eth-ropsten.alchemyapi.io/v2/cidKix2Xr-snU3f6f6Zjq_rYdalKKHmW',
   [ChainId.RINKEBY]: 'https://eth-rinkeby.alchemyapi.io/v2/XVLwDlhGP6ApBXFz_lfv0aZ6VmurWhYD',
   [ChainId.GÖRLI]: 'https://eth-goerli.alchemyapi.io/v2/Dkk5d02QjttYEoGmhZnJG37rKt8Yl3Im',
